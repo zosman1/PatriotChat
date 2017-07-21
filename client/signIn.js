f6b8114d01dd93dabcf6a082ee9b2c4be31b536c
Zach Osman <zosman@gmu.edu>
2017-07-20 23:28:44 -0400

    Creating sample user object

--- client/signIn.js
+++ client/signIn.js
@@ -3,14 +3,28 @@ import { Text, View, Button, AsyncStorage } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 export default class SignIn extends Component {
-    static navigationOptions = {
-        title: 'SignIn',
+  static navigationOptions = {
+      title: 'SignIn',
+  }
+  render() {
+    let user = {
+      username: 'zoz',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'jdoe5@gmu.edu',
+      netid: 'jdoe',
+      Gnumber: '00000000'
     }
-    render() {
-        return(
-            <View>
-                <Text>Signin</Text>
-            </View>
-        )
-    }
+    return(
+      <View>
+        <Text>Signin</Text>
+        <Button
+        title='Set Placeholder Values for user'
+        onPress={() => {
+          AsyncStorage.setItem('user', JSON.stringify(user));
+        }}
+        />
+      </View>
+    )
+  }
 }
